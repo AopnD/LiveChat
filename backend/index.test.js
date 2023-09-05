7054546f36dfb2301d9f87c0c61edd412901d365
Omer Malka <aopnd@outlook.com>
2023-09-05 16:42:15 +0300

    removed coments

--- backend/index.test.js
+++ backend/index.test.js
@@ -2,29 +2,27 @@ const request = require('supertest');
 const express = require('express');
 const app = express();
 
-// Middleware
 app.use(express.json());
 
-// Route for testing
+
 app.post('/authenticate', (req, res) => {
   const { username } = req.body;
   res.status(200).json({ username });
 });
 
-// Start the server before running tests
+
 const server = app.listen(3001, () => {
   console.log('Server up and running 😄');
 });
 
-// Define test cases
 describe('Authentication API', () => {
   afterAll(() => {
-    // Close the server after all tests are done
+
     server.close();
   });
 
   it('should create a new user with valid data', async () => {
-    const username = 'zack'; // Replace with the username you want to test
+    const username = 'zack'; 
 
     const response = await request(app)
       .post('/authenticate')
